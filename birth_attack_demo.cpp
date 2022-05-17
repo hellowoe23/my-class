36d18c9ddd3cf71075e739fb29cf361489fc0507
hellowoe23 <98643163+hellowoe23@users.noreply.github.com>
2022-05-17 16:12:20 +0800

    Update birth_attack_demo.cpp

--- birth_attack_demo.cpp
+++ birth_attack_demo.cpp
@@ -12,47 +12,37 @@ unsigned char* sm3(unsigned char* msg)
    unsigned int len = 0;
    unsigned char* outdigest = NULL;
     unsigned char *ret=(unsigned char*)malloc(32*sizeof(unsigned char*));
-    /* Create a context for the digest operation */
+  
     ctx = EVP_MD_CTX_new();
     if (ctx == NULL)
         goto err;
 
-    /*
-     * Fetch the SHA256 algorithm implementation for doing the digest. We're
-     * using the "default" library context here (first NULL parameter), and
-     * we're not supplying any particular search criteria for our SHA256
-     * implementation (second NULL parameter). Any SHA256 implementation will
-     * do.
-     */
+  
     SM3 = EVP_MD_fetch(NULL, "SM3", NULL);
     if (SM3 == NULL)
         goto err;
 
-    /* Initialise the digest operation */
+   
     if (!EVP_DigestInit_ex(ctx, SM3, NULL))
         goto err;
 
-    /*
-     * Pass the message to be digested. This can be passed in over multiple
-     * EVP_DigestUpdate calls if necessary
-     */
+  
     if (!EVP_DigestUpdate(ctx, msg, sizeof(msg)))
         goto err;
 
-    /* Allocate the output buffer */
     outdigest = (unsigned char*)OPENSSL_malloc(EVP_MD_get_size(SM3));
     if (outdigest == NULL)
         goto err;
 
-    /* Now calculate the digest itself */
+
     if (!EVP_DigestFinal_ex(ctx, outdigest, &len))
         goto err;
-    //printf("%d", len);
+   
      for (int i = 0; i < 32; i++)
     {
          ret[i] = outdigest[i];
     }
-    /* Print out the digest result */
+   
     //BIO_dump_fp(stdout, outdigest, len);
 
 err:
@@ -95,7 +85,7 @@ void birthday_attack(unsigned char* m)
    {
        if (temp[i] != temp1[i]) goto flag;
    }
-   printf("ǰ24λsm4��ײ��\n");
+   printf("Ç°24Î»sm4Åö×²£º\n");
    for (int i = 0; i < 32; i++)
    {
        printf("%02hx", btemp[i]);
